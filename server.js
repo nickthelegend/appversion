56ab56e6a3b5719bae6ff9f10b0883ab78140179
nickthelegend <82558763+nickthelegend@users.noreply.github.com>
2024-12-06 17:57:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.get('/', (req, res) => {
   // Respond with the JSON containing the latest version and update URL
   res.json({
-    latestVersion: '1.2.0',
+    latestVersion: '1.1.0',
     updateUrl: 'https://expo.dev/artifacts/eas/7jivTZKVdg4aWbusYDMAtL.apk',
   });
 });
